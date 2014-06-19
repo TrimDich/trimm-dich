ad50585140913ec08be302cf770066d674fcee7e
werner.eckert <werner.eckert@googlemail.com>
2014-06-19 16:04:18 +0200

    bla

---
+++ TrimmDich/src/de/lmu/msp/trimmdich/com/Formeln.java
@@ -0,0 +1,7 @@
+package de.lmu.msp.trimmdich.com;
+
+public class Formeln {
+	public static float calcCalories(float distance, float weight){
+		return distance*weight;
+	}
+}
--- TrimmDich/src/de/lmu/msp/trimmdich/exercise/ExerciseActivity.java
+++ TrimmDich/src/de/lmu/msp/trimmdich/exercise/ExerciseActivity.java
@@ -1,5 +1,7 @@
 package de.lmu.msp.trimmdich.exercise;
 
+import java.io.File;
+
 import de.lmu.msp.trimmdich.R;
 import de.lmu.msp.trimmdich.R.layout;
 import de.lmu.msp.trimmdich.com.Safe;
@@ -10,6 +12,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 
@@ -25,6 +28,7 @@ public class ExerciseActivity extends Activity implements SensorEventListener{
 		safe = Safe.getInstance();
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		setContentView(R.layout.activity_exercise);
+		Log.d(TAG,"DIRECTORY_DOWNLOADS: "+Environment.DIRECTORY_DOWNLOADS);
 	}
 	protected void onStop() {
 		sensorManager.unregisterListener(this);
@@ -37,8 +41,10 @@ public class ExerciseActivity extends Activity implements SensorEventListener{
 	
 	public void onStartBtPressed(View view){
 		Log.w(TAG,"onStartBtPress");
-		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+		if(isExternalStorageWritable()){
+			sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+			sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+		}
 	}
 	public void onStopBtPressed(){
 		Log.w(TAG,"onStopBtPress");
@@ -65,4 +71,24 @@ public class ExerciseActivity extends Activity implements SensorEventListener{
 			break;
 		}
 	}
+	
+	/*Methoden für Erhebung der Testdaten und Speicherung*/
+	/* Checks if external storage is available for read and write */
+	public boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+	public File getStorageDir(String albumName) {
+	    // Get the directory for the user's public pictures directory.
+	    File file = new File(Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_DOWNLOADS), albumName);
+	    if (!file.mkdirs()) {
+	        Log.e(TAG, "Directory not created");
+	    }
+	    return file;
+	}
+
 }
