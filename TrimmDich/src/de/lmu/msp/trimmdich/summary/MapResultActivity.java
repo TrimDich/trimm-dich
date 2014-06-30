e3b60f88d4810b213bb8217e0b622b40857f991c
Sebastian Wagner <s.t.wagner@gmx.de>
2014-06-30 11:35:11 +0200

    Added logic to Statistics activity, minor changes to MapResultActivity, added strings.

--- TrimmDich/src/de/lmu/msp/trimmdich/summary/MapResultActivity.java
+++ TrimmDich/src/de/lmu/msp/trimmdich/summary/MapResultActivity.java
@@ -2,11 +2,12 @@ package de.lmu.msp.trimmdich.summary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import de.lmu.msp.trimmdich.R;
 
 public class MapResultActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_);
+		setContentView(R.layout.activity_mapresult);
 	}
 }
--- TrimmDich/src/de/lmu/msp/trimmdich/summary/StatisticsActivity.java
+++ TrimmDich/src/de/lmu/msp/trimmdich/summary/StatisticsActivity.java
@@ -1,12 +1,32 @@
 package de.lmu.msp.trimmdich.summary;
 
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import de.lmu.msp.trimmdich.R;
 
 public class StatisticsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_statistics);
+		
+		TextView distanceMetric = (TextView) findViewById(R.id.distanceMetric);
+		TextView squatMetric = (TextView) findViewById(R.id.squatMetric);
+		TextView speedMetric = (TextView) findViewById(R.id.speedMetric);
+		//Dummy values
+		double distanceRun = 5.0;
+		double timeTaken = 20.0;
+		double speed = distanceRun / timeTaken;
+		int squatReps = 30;
+		String distanceText = String.valueOf(distanceRun) + " m";
+		String squatText = String.valueOf(squatReps);
+		String speedText = String.valueOf(speed) + " km/h";
+		//Set text inside view
+		distanceMetric.setText(distanceText);
+		speedMetric.setText(speedText);
+		squatMetric.setText(squatText);
+		
 	}
 }
