b4dbfed11126fd2af60238a8eacfb3707e28ed89
Sebastian Wagner <s.t.wagner@gmx.de>
2014-07-03 13:39:58 +0200

    Added map display function to MapResultActivity. (Dummy without values)

--- TrimmDich/src/de/lmu/msp/trimmdich/summary/MapResultActivity.java
+++ TrimmDich/src/de/lmu/msp/trimmdich/summary/MapResultActivity.java
@@ -1,13 +1,45 @@
 package de.lmu.msp.trimmdich.summary;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 import android.app.Activity;
 import android.os.Bundle;
 import de.lmu.msp.trimmdich.R;
+import de.lmu.msp.trimmdich.data.Location;
+import de.lmu.msp.trimmdich.data.Route;
 
 public class MapResultActivity extends Activity {
+	
+	GoogleMap map;
+	Route route;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mapresult);
+		
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+		        .getMap();
+		
+		// Fetch the runned route from the previous activities
+		
+		route = null;
+		
+		// add route to map
+		PolylineOptions line = new PolylineOptions();
+		
+		for (Location location : route.locations) {
+			line.add(location.location);
+			map.addMarker(new MarkerOptions().position(location.location).title("location"));
+		}
+		map.addPolyline(line);
+		
+			
 	}
+	
 }
+
