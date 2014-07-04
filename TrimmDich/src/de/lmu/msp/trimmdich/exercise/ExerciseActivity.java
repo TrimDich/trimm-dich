cf77e7cb438cc52c57ab91c2c3b22e0c7c9ebbe3
werner.eckert <werner.eckert@googlemail.com>
2014-07-04 12:30:43 +0200

    layout Anpassungen

--- TrimmDich/src/de/lmu/msp/trimmdich/exercise/ExerciseActivity.java
+++ TrimmDich/src/de/lmu/msp/trimmdich/exercise/ExerciseActivity.java
@@ -1,12 +1,9 @@
 package de.lmu.msp.trimmdich.exercise;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Locale;
 
 import de.lmu.msp.trimmdich.R;
-import de.lmu.msp.trimmdich.R.layout;
-import de.lmu.msp.trimmdich.main.SetupActivity;
 
 import android.app.Activity;
 import android.hardware.Sensor;
@@ -20,15 +17,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import de.lmu.msp.trimmdich.R;
 
 public class ExerciseActivity extends Activity implements SensorEventListener,
 		OnInitListener {
 	public static final String TAG = "TD-Exercise";
-	private static double BIAS = 10.;
 
 	private SensorManager sensorManager;
-	private FileOutputStream fos;
 	private TextToSpeech tts;
 
 	private SquatCounter squatCounter;
@@ -40,6 +34,7 @@ public class ExerciseActivity extends Activity implements SensorEventListener,
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		setContentView(R.layout.activity_exercise);
 		countView = (TextView) findViewById(R.id.exercise_countView);
+		setTitle(R.string.exercise_title);
 	}
 
 	@Override
@@ -72,38 +67,6 @@ public class ExerciseActivity extends Activity implements SensorEventListener,
 		sensorManager.registerListener(this,
 				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
 				SensorManager.SENSOR_DELAY_NORMAL);
-		// if(isExternalStorageWritable()){
-		//
-		// try {
-		// String filename = "trimmdich_"+System.currentTimeMillis()+".txt";
-		// File myFile = new File(Environment.getExternalStorageDirectory(),
-		// filename);
-		// if (!myFile.exists())
-		// myFile.createNewFile();
-		// fos = new FileOutputStream(myFile);
-		// } catch (Exception e){
-		// e.printStackTrace();
-		// }
-		// alphaFilter = new AlphaFilter();
-		// goleyFilter = new GolayFilter();
-		// goleyFilterVektor = new GolayFilter();
-		// kalmanFilter = new KalmanFilter(1, 1, 1, 1);
-		// mittel21 = new Mittelung21Grad();
-		// mittel3 = new Mittelung3Grad();
-		// mittel5 = new Mittelung5Grad();
-		// mittel5Vektor = new Mittelung5Grad();
-		// zentral11 = new Zentralwert11();
-		// zentral11Vektor = new Zentralwert11();
-		// zentral21 = new Zentralwert21();
-		// zentral3 = new Zentralwert3();
-		// zentral5 = new Zentralwert5();
-		// sensorManager.registerListener(this,
-		// sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-		// SensorManager.SENSOR_DELAY_NORMAL);
-		// //sensorManager.registerListener(this,
-		// sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
-		// SensorManager.SENSOR_DELAY_NORMAL);
-		// }
 		Toast.makeText(this, "START", Toast.LENGTH_SHORT).show();
 	}
 
@@ -139,21 +102,6 @@ public class ExerciseActivity extends Activity implements SensorEventListener,
 	GolayFilter goleyFilter;
 	GolayFilter goleyFilterVektor;
 
-	// float[] gyroVal = {0,0,0};
-	// AlphaFilter alphaFilter;
-	// GolayFilter goleyFilter;
-	// GolayFilter goleyFilterVektor;
-	// KalmanFilter kalmanFilter;
-	// Mittelung21Grad mittel21;
-	// Mittelung3Grad mittel3;
-	// Mittelung5Grad mittel5;
-	// Mittelung5Grad mittel5Vektor;
-	// Zentralwert11 zentral11;
-	// Zentralwert11 zentral11Vektor;
-	// Zentralwert21 zentral21;
-	// Zentralwert3 zentral3;
-	// Zentralwert5 zentral5;
-
 	private int squadCount = 0;
 
 	@Override
@@ -178,42 +126,6 @@ public class ExerciseActivity extends Activity implements SensorEventListener,
 				tts.speak(getString(R.string.exercise_end),
 						TextToSpeech.QUEUE_FLUSH, null);
 		}
-
-		// switch(event.sensor.getType()){
-		// case Sensor.TYPE_ACCELEROMETER:
-		// if(firstTime == 0)
-		// firstTime = event.timestamp;
-		//
-		// //String string =
-		// ((double)(event.timestamp-firstTime)/1000000000.)+";"+event.values[0]+";"+event.values[1]+";"+event.values[2]+";"+gyroVal[0]+";"+gyroVal[1]+";"+gyroVal[2]+"\n";
-		// double vektor =
-		// Math.sqrt(event.values[0]*event.values[0]+event.values[1]*event.values[1]+event.values[2]*event.values[2]);
-		// double goley = goleyFilterVektor.calcGolay(vektor);
-		// String string = (
-		// (double)(event.timestamp-firstTime)/1000000000.)+";"+event.values[0]+";"+event.values[1]+";"+event.values[2]+";"+
-		// alphaFilter.calcAlpha(event.values[2])+";"+goleyFilter.calcGolay(event.values[2])+";"+kalmanFilter.calcKalman(event.values[2])+";"+
-		// mittel21.calc21Middle(event.values[2])+";"+mittel3.calc3Middle(event.values[2])+";"+mittel5.calc5Middle(event.values[2])+";"+
-		// zentral11.calc11Zentral(event.values[2])+";"+zentral21.calc21Zentral(event.values[2])+";"+zentral3.calc3Zentral(event.values[2])+";"+zentral5.calc5Zentral(event.values[2])+";"+
-		// goley+";"+mittel5Vektor.calc5Middle(goley)+";"+zentral11Vektor.calc11Zentral(vektor)+"\n";
-		// string = string.replace('.', ',');
-		// Log.d(TAG, string);
-		// byte[] data = string.getBytes();
-		// try {
-		// fos.write(data);
-		// fos.flush();
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// break;
-		// case Sensor.TYPE_GYROSCOPE:
-		// gyroVal = event.values;
-		// Log.d(TAG,event.timestamp+" ::GYR:: "+event.values[0]+"|"+event.values[1]+"|"+event.values[2]);
-		// break;
-		// default:
-		// Log.d(TAG,event.timestamp+" ::???:: Type= "+event.sensor.getType());
-		// break;
-		// }
 	}
 
 	@Override
