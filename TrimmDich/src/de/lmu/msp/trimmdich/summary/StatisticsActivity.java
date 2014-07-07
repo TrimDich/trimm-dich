abea673000b21929b0326aa0521f448cba879333
Sebastian Wagner <s.t.wagner@gmx.de>
2014-07-07 10:16:30 +0200

    Minor changes to Statistics.

--- TrimmDich/src/de/lmu/msp/trimmdich/summary/StatisticsActivity.java
+++ TrimmDich/src/de/lmu/msp/trimmdich/summary/StatisticsActivity.java
@@ -43,7 +43,9 @@ public class StatisticsActivity extends Activity {
 		//double timeTaken = 1.0;
 		//double speed = 0.0 / timeTaken;
 		int squatReps = 0;
-		
+		int dipReps = 0;
+		int pullupReps = 0;
+		int pushupReps = 0;
 		
 		
 		//The total distance that was run
@@ -84,18 +86,37 @@ public class StatisticsActivity extends Activity {
 				Log.v("Route Locations", "There is a location.");
 				for(Exercise e: l.selectedExercises)
 				{
+					switch(e.getType())
+					{
+						case SQUATS:
+							squatReps += e.getRepetitionsActual();
+							break;
+						case DIPS:
+							dipReps += e.getRepetitionsActual();
+							break;
+						case PULL_UP:
+							pullupReps += e.getRepetitionsActual();
+							break;
+						case PUSH_UP:
+							pushupReps += e.getRepetitionsActual();
+							break;	
+						default:
+							break;
+					}
+					Log.v("Actual repitions",String.valueOf(e.getType()));
 					Log.v("Actual repitions",String.valueOf(e.getRepetitionsActual()));
 					Log.v("Goal repitions",String.valueOf(e.getRepetitionsGoal()));
-					squatReps += e.getRepetitionsActual();
+					
 				}
 		    }
 		}
 		Log.v("Total Distance", String.valueOf(mWorkoutTracker.round(totalDistance, 1)));
 		Log.v("Hour Difference", String.valueOf(diffInHours));
+		//durch 1000 da distance in m angegeben
 		double speed = mWorkoutTracker.round(totalDistance / diffInHours / 1000.0, 1);
 		String distanceText = String.valueOf(mWorkoutTracker.round(totalDistance, 1)) + " m";
 		distanceMetric.setText(distanceText);
-		String squatText = String.valueOf(squatReps);
+		String squatText = String.valueOf(squatReps + " / " + pullupReps+" / "+pushupReps+" / "+dipReps);
 		String speedText = String.valueOf(speed) + " km/h";
 		// Set text inside view		
 		speedMetric.setText(speedText);
